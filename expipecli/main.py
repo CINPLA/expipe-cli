e494505f352c4db413d940b78e6928685441bd9a
Mikkel Elle Lepperød <lep.mik@gmail.com>
2017-04-12 08:56:37 +0200

    add config

--- expipecli/main.py
+++ expipecli/main.py
@@ -18,12 +18,12 @@ import click
 from six import exec_
 import glob
 
-from .utils import discover_plugins
+from .utils import discover_plugins, IPlugin
 
 
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 # CLI tool
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 
 @click.group()
 # @click.version_option(version=__version_git__)
@@ -31,13 +31,45 @@ from .utils import discover_plugins
 @click.pass_context
 def expipe(ctx, pdb=None):
     """Add subcommands to 'expipe' with plugins
-    using `attach_to_cli()` and the `click` library."""
+    using `attach_to_cli()` and the `click` library.
+
+    Note that you can get help from a COMMAND by "expipe COMMAND --help"
+    """
     pass
 
 
-#------------------------------------------------------------------------------
+class Default(IPlugin):
+    def attach_to_cli(self, cli):
+        @cli.command('configure')
+        @click.option('--data-path',
+                      type=click.STRING,
+                      help='Path to where data files should be stored',
+                      )
+        @click.option('--email',
+                      type=click.STRING,
+                      help='User email on Firebase server',
+                      )
+        @click.option('--password',
+                      type=click.STRING,
+                      help='User password on Firebase server (WARNING: Will be stored in plain text!)',
+                      )
+        @click.option('--url_prefix',
+                      type=click.STRING,
+                      help='Prefix of Firebase server URL (https://<url_prefix>.firebaseio.com)',
+                      )
+        @click.option('--api_key',
+                      type=click.STRING,
+                      help='Firebase API key',
+                      )
+        def configure(data_path, email, password, url_prefix, api_key):
+            """Create a configuration file."""
+            import expipe
+            expie.configure(data_path, email, password, url_prefix, api_key)
+
+
+# ------------------------------------------------------------------------------
 # CLI plugins
-#------------------------------------------------------------------------------
+# ------------------------------------------------------------------------------
 
 
 def load_cli_plugins(cli, config_dir=None):
