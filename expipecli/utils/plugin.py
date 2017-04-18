b6026f95d4ccf54967131681d7015a69fdea47f8
Mikkel Elle Lepperød <lep.mik@gmail.com>
2017-04-18 11:49:16 +0200

    fix plugin bug

--- expipecli/utils/plugin.py
+++ expipecli/utils/plugin.py
@@ -66,7 +66,7 @@ def discover_plugins():
     for path in paths:
         exs.extend(glob.glob(op.join(path, 'plugin-expipe-*')))
     if len(exs) == 0:
-        return
+        return IPluginRegistry.plugins
     # TODO reveal plugin module in a non ugly way
     for executable in exs:
         process = subprocess.check_output(executable, shell=True)
