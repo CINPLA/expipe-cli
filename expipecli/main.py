b37b43817a6032f387353f4a6236a3f3e5f28096
Mikkel E Lepperød <lep.mik@gmail.com>
2018-10-23 21:09:52 +0200

    implement discovery and set user and global config

--- expipecli/main.py
+++ expipecli/main.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-# flake8: noqa
 
 """CLI tool."""
 
@@ -15,7 +14,71 @@ import click
 import pathlib
 from .utils import discover_plugins, IPlugin
 import expipe as expipe_module
+try:
+    import ruamel.yaml as yaml
+except ImportError:
+    import ruamel_yaml as yaml
 
+
+def yaml_dump(f, data):
+    assert f.suffix == '.yaml'
+    with f.open("w", encoding="utf-8") as fh:
+        yaml.dump(
+            data, fh,
+            default_flow_style=False,
+            allow_unicode=True,
+            Dumper=yaml.RoundTripDumper
+        )
+
+
+def yaml_get(path):
+    if not path.exists():
+        return None
+    with path.open('r', encoding='utf-8') as f:
+        result = yaml.load(f, Loader=yaml.Loader)
+    return result
+
+
+def load_local_config(path):
+    current_root = pathlib.Path(path)
+    current_path = current_root / "expipe.yaml"
+    if not current_path.exists():
+        if current_root.match(current_path.root):
+            return None, None, {}
+
+        return load_local_config(current_root.parent)
+
+    return current_root, current_path, yaml_get(current_path)
+
+
+def load_config(project_id=None):
+    # make paths
+    cwd = pathlib.Path('').cwd()
+    global_root = cwd.home() / '.config' / 'expipe'
+    global_config_path = global_root / 'expipe-config.yaml'
+    global_config = yaml_get(global_config_path) or {}
+    # see if you are in a filesystem project
+    local_root, local_path, local_config = load_local_config(cwd)
+    if local_root is not None:
+        assert project_id is None, 'project_id should not be given if in a filesystem project'
+        project_id = local_root.stem
+    assert project_id is not None, 'project_id should be given if not in a filesystem project'
+    user_root = global_root / project_id
+    user_config_path = (user_root / project_id).with_suffix('.yaml')
+    user_config = yaml_get(user_config_path) or {}
+    config = {
+        'global': global_config,
+        'global_path': global_config_path,
+        'global_root': global_root,
+        'user': user_config,
+        'user_path': user_config_path,
+        'user_root': user_root,
+        'local': local_config,
+        'local_path': local_path,
+        'local_root': local_root,
+    }
+
+    return config
 
 # ------------------------------------------------------------------------------
 # CLI tool
@@ -32,8 +95,6 @@ def expipe(ctx):
     Note that you can get help from a COMMAND by "expipe COMMAND --help"
     """
     pass
-    #TODO discovery
-    #
 
 
 class Default(IPlugin):
@@ -42,14 +103,87 @@ class Default(IPlugin):
         @click.argument(
             'project-id', type=click.STRING
         )
-        def init(project_id):
+        def create(project_id):
             """Create a project."""
             cwd = os.getcwd()
+            root, _, _ = load_local_config(cwd)
+            if root is not None:
+                print(
+                    'Cannot create a project inside a project. ' +
+                    'You are currently in "{}"'.format(root)
+                )
+                return
             path = pathlib.Path(cwd) / project_id
             # server = expipe.load_filesystem(path.parent)
             # project = server.require_project(project_id)
             expipe_module.create_project(path)
 
+        @cli.command('status')
+        def status():
+            """Print project status."""
+            config = load_config()
+            if config['local'] == {}:
+                print('Unable to locate expipe configurations.')
+                return
+            assert config['local']['type'] == 'project'
+            # server = expipe.load_filesystem(path.parent)
+            # project = server.require_project(project_id)
+            project = expipe_module.get_project(config['local_root'])
+            print('Local configuration:')
+            for k, v in config['local'].items():
+                print('\t{}: {}'.format(k, v))
+            print()
+            print('User configuration:')
+            for k, v in config['user'].items():
+                print('\t{}: {}'.format(k, v))
+            print()
+            print('Global configuration:')
+            for k, v in config['global'].items():
+                print('\t{}: {}'.format(k, v))
+
+        @cli.command('set')
+        @click.option(
+            '--project-id', type=click.STRING,
+        )
+        @click.option(
+            '--user', type=click.STRING,
+        )
+        @click.option(
+            '--email', type=click.STRING,
+        )
+        @click.option(
+            '--location', type=click.STRING,
+        )
+        def set(project_id, **kw):
+            """Create a project."""
+            config = load_config(project_id)
+            if config['local'] == {}:
+                print('Current location is not recognized as a expipe project, aborting...')
+                return
+            assert config['local']['type'] == 'project'
+            # server = expipe.load_filesystem(path.parent)
+            # project = server.require_project(project_id)
+            project = expipe_module.get_project(config['local_root'])
+            config['user'].update({k: v for k,v in kw.items() if v})
+            config['user_root'].mkdir(exist_ok=True)
+            yaml_dump(config['user_path'], config['user'])
+
+        @cli.command('set-global')
+        @click.option(
+            '--user', type=click.STRING,
+        )
+        @click.option(
+            '--email', type=click.STRING,
+        )
+        @click.option(
+            '--location', type=click.STRING,
+        )
+        def set(**kw):
+            """Create a project."""
+            config = load_config()
+            config['global'].update({k: v for k,v in kw.items() if v})
+            config['global_root'].mkdir(exist_ok=True)
+            yaml_dump(config['global_path'], config['global'])
 
 # ------------------------------------------------------------------------------
 # CLI plugins
@@ -61,12 +195,12 @@ def load_cli_plugins(cli, config_dir=None):
 
     plugins = discover_plugins()
     for plugin in plugins:
-        if not hasattr(plugin, 'attach_to_cli'):  # pragma: no cover
+        if not hasattr(plugin, 'attach_to_cli'):
             continue
         # NOTE: plugin is a class, so we need to instantiate it.
         try:
             plugin().attach_to_cli(cli)
-        except Exception as e:  # pragma: no cover
+        except Exception as e:
             print("Error when loading plugin `%s`" % plugin)
             raise e
 
