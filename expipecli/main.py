139ff9f9ab20ae49b1ba92808dcfaee5f6273743
Mikkel Elle Lepperød <lep.mik@gmail.com>
2018-10-26 17:13:52 +0200

    conform to latest api

--- expipecli/main.py
+++ expipecli/main.py
@@ -38,7 +38,7 @@ def yaml_get(path):
         result = yaml.load(f, Loader=yaml.Loader)
     return result
 
-
+# TODO move to expipe
 def load_local_config(path):
     current_root = pathlib.Path(path)
     current_path = current_root / "expipe.yaml"
@@ -58,13 +58,13 @@ def load_global_config():
     return global_root, global_config_path, global_config
 
 
-def load_user_config(project_id=None):
+def load_project_config(project_id=None):
     if project_id is None:
         return None, None, {}
-    user_root = pathlib.Path.home() / '.config' / 'expipe' / project_id
-    user_config_path = (user_root / project_id).with_suffix('.yaml')
-    user_config = yaml_get(user_config_path) or {}
-    return user_root, user_config_path, user_config
+    project_root = pathlib.Path.home() / '.config' / 'expipe' / project_id
+    project_config_path = (project_root / project_id).with_suffix('.yaml')
+    project_config = yaml_get(project_config_path) or {}
+    return project_root, project_config_path, project_config
 
 
 def load_config(project_id=None):
@@ -75,14 +75,14 @@ def load_config(project_id=None):
     if local_root is not None:
         assert project_id is None, '"project-id" should not be given if in a filesystem project'
         project_id = local_root.stem
-    user_root, user_config_path, user_config = load_user_config(project_id)
+    project_root, project_config_path, project_config = load_project_config(project_id)
     config = {
         'global': global_config,
         'global_path': global_config_path,
         'global_root': global_root,
-        'user': user_config,
-        'user_path': user_config_path,
-        'user_root': user_root,
+        'project': project_config,
+        'project_path': project_config_path,
+        'project_root': project_root,
         'local': local_config,
         'local_path': local_path,
         'local_root': local_root,
@@ -123,8 +123,7 @@ class Default(IPlugin):
                     'You are currently in "{}"'.format(root)
                 )
                 return
-            server = expipe_module.load_file_system(root=cwd)
-            server.create_project(project_id)
+            expipe_module.load_project(path=cwd / project_id)
 
         @cli.command('status') # TODO add project id and spcialize firebase printing to not reveal confidential info
         def status():
@@ -134,21 +133,39 @@ class Default(IPlugin):
                 print('Unable to locate expipe configurations.')
                 return
             assert config['local']['type'] == 'project'
-            server = expipe_module.load_file_system(root=config['local_root'].parent)
-            server.get_project(config['local_root'].stem)
+            server = expipe_module.load_project(path=config['local_root'])
             print('Local configuration:')
             for k, v in config['local'].items():
                 print('\t{}: {}'.format(k, v))
             print()
-            print('User configuration:')
-            for k, v in config['user'].items():
+            print('project configuration:')
+            for k, v in config['project'].items():
                 print('\t{}: {}'.format(k, v))
             print()
             print('Global configuration:')
             for k, v in config['global'].items():
                 print('\t{}: {}'.format(k, v))
 
-        @cli.command('set-user')
+        @cli.command('list')
+        @click.argument(
+            'object-type', type=click.Choice(['actions', 'entities', 'modules'])
+        )
+        def list_stuff(object_type):
+            """Print project objects."""
+            config = load_config()
+            if config['local_root'] is None:
+                print('Unable to locate expipe configurations.')
+                return
+            assert config['local']['type'] == 'project'
+            project = expipe_module.load_project(path=config['local_root'])
+            for object in getattr(project, object_type):
+                print(object)
+
+
+        @cli.command('config') # TODO add stuff
+        @click.argument(
+            'target', type=click.Choice(['global', 'project', 'local'])
+        )
         @click.option(
             '--project-id', type=click.STRING,
         )
@@ -164,43 +181,21 @@ class Default(IPlugin):
         @click.option(
             '--plugin', '-p', type=click.STRING, multiple=True
         )
-        def set_user(project_id, plugin, **kw):
-            """Set local user info."""
+        def set_config(project_id, plugin, target, **kw):
+            """Set config info."""
             config = load_config(project_id)
-            if config['user_root'] is None:
+            if config[target + '_root'] is None:
                 print(
-                    'Unable to load user config, move into a project or ' +
+                    'Unable to load config, move into a project or ' +
                     'give "project-id" explicitly.')
                 return
             if len(plugin) > 0:
                 plugins = [p for p in plugin]
                 kw['plugins'] = list(set(plugins))
-            config['user'].update({k: v for k,v in kw.items() if v})
-            config['user_root'].mkdir(exist_ok=True)
-            yaml_dump(config['user_path'], config['user'])
+            config[target].update({k: v for k,v in kw.items() if v})
+            config[target + '_root'].mkdir(exist_ok=True)
+            yaml_dump(config[target + '_path'], config[target])
 
-        @cli.command('set-global')
-        @click.option(
-            '--username', type=click.STRING,
-        )
-        @click.option(
-            '--email', type=click.STRING,
-        )
-        @click.option(
-            '--location', type=click.STRING,
-        )
-        @click.option(
-            '--plugin', '-p', type=click.STRING, multiple=True
-        )
-        def set_global(plugin, **kw):
-            """Set global user info."""
-            config = load_config()
-            if len(plugin) > 0:
-                plugins = [p for p in plugin]
-                kw['plugins'] = list(set(plugins))
-            config['global'].update({k: v for k,v in kw.items() if v})
-            config['global_root'].mkdir(exist_ok=True)
-            yaml_dump(config['global_path'], config['global'])
 
 # ------------------------------------------------------------------------------
 # CLI plugins
@@ -229,11 +224,11 @@ def list_plugins():
     # see if you are in a filesystem project
     local_root, local_path, local_config = load_local_config(cwd)
     if local_root is not None:
-        user_root, user_config_path, user_config = load_user_config(local_root.stem)
-        user_plugins = user_config.get('plugins') or []
+        project_root, project_config_path, project_config = load_project_config(local_root.stem)
+        project_plugins = project_config.get('plugins') or []
     else:
-        user_plugins = []
+        project_plugins = []
     global_plugins = global_config.get('plugins') or []
-    return global_plugins + user_plugins
+    return global_plugins + project_plugins
 
 load_cli_plugins(expipe, list_plugins())
